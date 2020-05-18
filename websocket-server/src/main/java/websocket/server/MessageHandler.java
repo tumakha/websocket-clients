847dd6cf04b652d7e85daf69bd3180be17862047
Yuriy Tumakha <tumakha@gmail.com>
2020-05-18 22:26:28 +0100

    MessageHandler

--- websocket-server/src/main/java/websocket/server/MessageHandler.java
+++ websocket-server/src/main/java/websocket/server/MessageHandler.java
@@ -1,27 +1,33 @@
 package websocket.server;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import lombok.SneakyThrows;
 import websocket.server.json.JsonSupport;
 import websocket.server.model.RequestMsg;
 import websocket.server.model.ResponseMsg;
 
-import static java.util.stream.IntStream.rangeClosed;
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
 
 /**
  * @author Yuriy Tumakha
  */
 public interface MessageHandler extends JsonSupport {
 
+  @SneakyThrows
   default void readMessage(ChannelHandlerContext ctx, TextWebSocketFrame textFrame) {
     String request = textFrame.text();
     System.out.println("New Test Request: " + request);
     RequestMsg requestMsg = fromJson(request, RequestMsg.class);
 
-    rangeClosed(1, requestMsg.getRequestMessages()).forEach(i -> {
-      String response = toJson(new ResponseMsg(i, System.nanoTime()));
-      ctx.channel().writeAndFlush(new TextWebSocketFrame(response));
-    });
+    Channel channel = ctx.channel();
+    int count = requestMsg.getRequestMessages();
+    for (int id = 1; id <= count; id++) {
+      String response = toJson(new ResponseMsg(id, System.nanoTime()));
+      channel.writeAndFlush(new TextWebSocketFrame(response));
+      MICROSECONDS.sleep(100); // run every 100 us
+    }
   }
 
 }
